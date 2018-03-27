efc22db3ed9c38db6a7a1009454f1523f7f7630b
Vinchurkar <siddhantvinchurkar@gmail.com>
2018-03-27 21:24:34 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<title>Haunting Hollows</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+	</head>
+	<body>
+
+		<!-- Header -->
+			<section id="header">
+				<div class="inner">
+					<span class="icon major fa-cloud"></span>
+					<h1>Hi, I'm <strong>Haunting Hollows</strong>.<br />
+					It looks as though everything is functioning the way it should.</h1>
+				<p>Apache/2.4.29 (Ubuntu) Server at <a href="http://13.127.95.197" target="_blank">13.127.95.197</a> Port 443</p>
+					<ul class="actions">
+						<li><a href="#one" class="button scrolly">What's hosted on this server?</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="main style1">
+				<div class="container">
+					<div class="row 150%">
+						<div class="6u 12u$(medium)">
+							<header class="major">
+								<h2>What's hosted on Haunting Hollows?</h2>
+							</header>
+							<p>Currently, the following websites are hosted by Haunting Hollows &rarr;</p>
+							<p><a href="https://onyxai.ga" target="_blank">Onyx AI</a></p>
+							<p><a href="https://sotf.in" target="_blank">Survival Of The Fittest (SOTF)</a></p>
+							<p><a href="https://hauntinghollows.ga">Haunting Hollows (This Website)</a></p>
+							<p><a href="https://siddhantvinchurkar.me" target="_blank">It's me!</a></p>
+							<p><a href="https://volatile.ga" target="_blank">Volatile, Inc.</a></p>
+						</div>
+						<div class="6u$ 12u$(medium) important(medium)">
+							<span class="image fit"><img src="images/ec2.jpg" alt="images/pic01.jpg" /></span>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<section id="footer">
+				<ul class="copyright">
+					<li>&copy; <script>document.write(new Date().getFullYear())</script> <a href="http://volatile.ga/">Volatile, Inc.</a> Designed by <a href="https://blog.siddhantvinchurkar.me/">Siddhant Vinchurkar</a>.</li>
+				</ul>
+			</section>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
